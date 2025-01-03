e8c485f786695a47f38eb70d63cc243ccdd7cafa
Ravindra Singh Rayal <rayalravi2001@gmail.com>
2025-01-03 06:06:47 +0530

    added anthropic ai/sdk

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,20 @@
 require("dotenv").config();
+
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropic = new Anthropic();
+
+async function main() {
+    const msg = await anthropic.messages.create({
+        model: "claude-3-5-sonnet-20241022",
+        max_tokens: 8192,
+        temperature: 0,
+        messages: [{
+            role : "user",
+            content : "what is 2 + 2"
+        }]
+      });
+      console.log(msg);
+}
+
+main();
